f20672bd744bb78e5d7b13e96a52c0e0ed8f254b
AAYUSH MISHRA <mishraji2112007@gmail.com>
2026-09-14 12:10:39 +0530

    Connect Django to Neon PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,6 @@
+import os
 from pathlib import Path
+import dj_database_url
 BASE_DIR=Path(__file__).resolve().parent.parent
 SECRET_KEY='django-insecure-student-attendance-mini-project-change-me'
 DEBUG = True
@@ -24,7 +26,19 @@ MIDDLEWARE = [
 ROOT_URLCONF='config.urls'
 TEMPLATES=[{'BACKEND':'django.template.backends.django.DjangoTemplates','DIRS':[BASE_DIR/'attendance'/'templates'],'APP_DIRS':True,'OPTIONS':{'context_processors':['django.template.context_processors.request','django.contrib.auth.context_processors.auth','django.contrib.messages.context_processors.messages']}}]
 WSGI_APPLICATION='config.wsgi.application'
-DATABASES={'default':{'ENGINE':'django.db.backends.sqlite3','NAME':BASE_DIR/'db.sqlite3'}}
+if os.environ.get("STORAGE_URL"):
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=os.environ.get("STORAGE_URL")
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 AUTH_PASSWORD_VALIDATORS=[]
 LANGUAGE_CODE='en-us'; TIME_ZONE='Asia/Kolkata'; USE_I18N=True; USE_TZ=True
 STATIC_URL = "/static/"
