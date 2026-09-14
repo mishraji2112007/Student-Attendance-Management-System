aa1229bae6fe9ee0a90b99658e4dd2c317d77371
AAYUSH MISHRA <mishraji2112007@gmail.com>
2026-09-14 11:13:09 +0530

    Fix Vercel allowed host

--- config/settings.py
+++ config/settings.py
@@ -2,7 +2,11 @@ from pathlib import Path
 BASE_DIR=Path(__file__).resolve().parent.parent
 SECRET_KEY='django-insecure-student-attendance-mini-project-change-me'
 DEBUG=True
-ALLOWED_HOSTS=[]
+ALLOWED_HOSTS = [
+    "student-attendance-management-syste-rouge.vercel.app",
+    "localhost",
+    "127.0.0.1",
+]
 INSTALLED_APPS=['django.contrib.admin','django.contrib.auth','django.contrib.contenttypes','django.contrib.sessions','django.contrib.messages','django.contrib.staticfiles','attendance']
 MIDDLEWARE=['django.middleware.security.SecurityMiddleware','django.contrib.sessions.middleware.SessionMiddleware','django.middleware.common.CommonMiddleware','django.middleware.csrf.CsrfViewMiddleware','django.contrib.auth.middleware.AuthenticationMiddleware','django.contrib.messages.middleware.MessageMiddleware','django.middleware.clickjacking.XFrameOptionsMiddleware']
 ROOT_URLCONF='config.urls'
